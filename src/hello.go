d29f212e20eb4250c4ca2e4fda7ba38d15c77e42
smuralee <suraj@surajmuraleedharan.com>
2020-02-25 10:28:20 +0000

    Added examples

--- src/hello.go
+++ src/hello.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!!")
+
+	fmt.Println("Sum : ", 2+6)
+	fmt.Println("Divide : ", 2/5)
+
+	var isValid = true
+	var isInvalid = false
+	fmt.Println("Boolean - true & false : ", isValid && isInvalid)
+	fmt.Println("Boolean - true or false : ", isValid || isInvalid)
+	fmt.Println("Boolean - negation true : ", !isValid)
+
 }
